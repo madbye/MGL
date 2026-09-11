98836a24889aa3ad4b4c4879b560e281782f1f8c
madbye <153489104+madbye@users.noreply.github.com>
2026-09-11 08:50:14 +0300

    Update DirectionalLight.cs

--- MGL/GFX/3D/Lights/DirectionalLight.cs
+++ MGL/GFX/3D/Lights/DirectionalLight.cs
@@ -5,5 +5,5 @@ namespace MGL.GFX._3D.Lights;
 public class DirectionalLight : Light
 {
     public Vector3 Direction { get; set; } = new(0, -1, 0);
-    public Vector3 Color { get; set; } = new(1.0f, 1.0f, 1.0f); // Яркий белый свет
+    public Vector3 Color { get; set; } = new(1.0f, 1.0f, 1.0f);
 }
